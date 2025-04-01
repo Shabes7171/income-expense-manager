22a2e11ede8dc04f195ad34db2f4a98e6d43f106
Shabes7171 <155089733+Shabes7171@users.noreply.github.com>
2025-04-01 18:22:49 +0530

    Update api.jsx

--- frontend/src/api.jsx
+++ frontend/src/api.jsx
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:5000';
+const API_URL = 'https://income-expense-manager-2.onrender.com';
 
 // Fetch stock data
 export const fetchStock = () => axios.get(`${API_URL}/stock`);
